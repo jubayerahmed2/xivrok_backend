f7944e60595d61a64ea8988acd9cb7df4161a885
jubayer <jubayerahmed.dev@gmail.com>
2025-09-02 10:22:37 +0600

    feat: add login user endpoint

--- src/controllers/auth/login.controller.js
+++ src/controllers/auth/login.controller.js
@@ -1,5 +1,8 @@
+// @ts-nocheck
 import { UserModel } from "../../models/user.model.js";
 import { ApiError } from "../../utils/api_error.js";
+import { ApiResponse } from "../../utils/api_response.js";
+import { asyncHandler } from "../../utils/async_handler.js";
 
 export const generateAccessAndRefreshToken = async (userId) => {
     try {
@@ -18,3 +21,55 @@ export const generateAccessAndRefreshToken = async (userId) => {
         );
     }
 };
+
+const loginUser = asyncHandler(async (req, res) => {
+    /*
+    -> get email and password from request body 
+    -> validate via zod 
+    -> check if email exist 
+    -> check password valid using - isPasswordCorrect method 
+    -> generate access and refresh token 
+    -> return response 
+    */
+
+    const { email, password } = req.body;
+
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+        throw new ApiError(404, "User with the email address not exist");
+    }
+
+    const isPasswordCorrect = await UserModel.isPasswordCorrect(password);
+
+    if (isPasswordCorrect) {
+        throw new ApiError(409, "Incorrect credential");
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+        user._id
+    );
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    };
+
+    const getUser = await UserModel.findById(user._id).select(
+        "-password -refreshToken"
+    );
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                { user: getUser, accessToken, refreshToken },
+                "LoggedIn successfully"
+            )
+        );
+});
+
+export { loginUser };
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import { loginUser } from "../controllers/auth/login.controller.js";
 import { registerUser } from "../controllers/auth/register.controller.js";
 import { verifyCode } from "../controllers/auth/verify_code.controller.js";
 import {
+    loginValidation,
     resisterValidation,
     verifyCodeValidation
 } from "../validation/user.validation.js";
@@ -9,6 +11,7 @@ import {
 const router = Router();
 
 router.route("/register").post(resisterValidation, registerUser);
+router.route("/login").post(loginValidation, loginUser);
 router.route("/verify-email").post(verifyCodeValidation, verifyCode);
 
 export default router;
